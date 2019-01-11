00a8a94b35a117ad84e8470260cbf551d65b4feb
Sebastian Tomczuk <sebastian.tomczuk5@gmail.com>
2019-01-11 22:14:26 +0100

    Done

---
+++ assignment_12/src/assignment_12/App.java
@@ -0,0 +1,49 @@
+package assignment_12;
+
+import ignore.TestingUtils;
+
+public class App {
+
+
+    /**
+     * Return a version of the given string, where for every star (*)
+     * in the string the star and the chars immediately to its left and right are gone.
+     * So "ab*cd" yields "ad" and "ab**cd" also yields "ad". 	<br>
+     * <br>
+     *
+     * <b>EXPECTATIONS:</b><br>
+     * starKill("cd*zq")  <b>---></b>"cq"<br>
+     * starKill("ab**cd")    <b>---></b> "ad" <br>
+     * starKill("wacy*xko") <b>---></b> "wacko" <br>
+     */
+
+    public static String starKill(String str) {
+        String newString = "";
+        for (int i = 0; i < str.length(); i++) {
+            char aChar = str.charAt(i);
+
+
+            if (i == 0 && aChar != '*') {
+
+                newString += aChar;
+            }
+
+            if (i > 0 && aChar != '*' && str.charAt(i - 1) != '*') {
+                newString += aChar;
+            }
+
+            if (i > 0 && aChar == '*' && str.charAt(i - 1) != '*') {
+                newString = newString.substring(0, newString.length() - 1);
+            }
+        }
+
+        return newString;
+    }
+
+
+    //----------------------STARTING POINT OF PROGRAM. IGNORE BELOW --------------------//
+    public static void main(String args[]) {
+        TestingUtils.runTests();
+
+    }
+}
